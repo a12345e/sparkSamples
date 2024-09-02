49e7f45626af259f3e976f0e449a5c73b6472841
Alon Ecker <a12345e@gmail.com>
2024-09-03 02:40:18 +0300

    fix

--- spark/find_match/find_match_ranges.py
+++ spark/find_match/find_match_ranges.py
@@ -6,154 +6,138 @@ import uuid
 
 from pyspark.sql.dataframe import DataFrame
 from enum import Enum
-class Status(Enum):
-      INVALID = 0
-      START = 1
-      UPDATE = 2
-      END = 3
+
+class FindMatchRange:
+    class Status(Enum):
+        INVALID = 0
+        START = 1
+        UPDATE = 2
+        END = 3
 
 
-class EndingTransactionReason(Enum):
-    MATCH_EXPECTED_END = 1
-    MATCH_END_FOR_UNEXPECTED_MATCH = 2
-    MATCH_END_FOR_UNEXPECTED_NULL = 3
-    MATCH_UNEXPECTED_START_DIFFERENT_MATCH = 4
-    MATCH_UNEXPECTED_START_SAME_MATCH = 5
+    class EndingTransactionReason(Enum):
+        MATCH_EXPECTED_END = 1
+        MATCH_END_FOR_UNEXPECTED_MATCH = 2
+        MATCH_END_FOR_UNEXPECTED_NULL = 3
+        MATCH_UNEXPECTED_START_DIFFERENT_MATCH = 4
+        MATCH_UNEXPECTED_START_SAME_MATCH = 5
 
-
-def initial_filter_non_nulls_a_col_time_col(df: DataFrame,
+    def initial_filter_non_nulls_a_col_time_col(self, df: DataFrame,
                     a_col: str,
                     time_col: str):
-    return df.where( (F.col(a_col).isNotNull()) & (F.col(time_col).isNotNull()))
+        return df.where( (F.col(a_col).isNotNull()) & (F.col(time_col).isNotNull()))
 
-def enumerate_status_column(df:DataFrame,
-                             status_col: str,
-                             status_values: Dict[str, Status]):
-    tmp_col =  f"{status_col}_{uuid.uuid4().hex}"
-    for status_value, status_enum in status_values.items():
-        df = df.withColumn(tmp_col,
+    @staticmethod
+    def enumerate_status_column(df:DataFrame,
+                                status_col: str,
+                                status_values: Dict[Status, str]) -> DataFrame:
+        tmp_col =  f"{status_col}_{uuid.uuid4().hex}"
+        df = df.withColumn(tmp_col,F.lit(None))
+        for status_enum, status_value in status_values.items():
+            df = df.withColumn(tmp_col,
                            F.when(F.col(status_col) == F.lit(status_value),
-                                  status_enum.value).otherwise(F.lit(Status.INVALID.value)))
-    return df.withColumn(status_col,F.col(tmp_col)).drop(tmp_col)
+                                  status_enum.value).otherwise(F.col(tmp_col)))
+        return df.withColumn(status_col,F.col(tmp_col)).drop(tmp_col)
 
-def keep_only_start_end_status(df: DataFrame, status_col: str):
-    return df.where(F.col(status_col).isin([Status.START.value, Status.END]))
+    def keep_only_start_end_status(self, df: DataFrame, status_col: str) -> DataFrame:
+        return df.where(F.col(status_col).isin([self.Status.START.value, self.Status.END.value]))
 
-def avoid_b_col_null_on_start_status(df: DataFrame, b_col: str, status_col: str):
-    df = df.where((F.col(status_col) != Status.START) |
+    def avoid_b_col_null_on_start_status(self, df: DataFrame, b_col: str, status_col: str) -> DataFrame:
+        return df.where((F.col(status_col) != self.Status.START.value) |
                   (F.col(b_col).isNotNull()))
 
-    return df.where(F.col(status_col).isin([Status.START.value, Status.END]))
 
 
-def keep_when_only_one_non_null_value_and_be_the_non_null_one_if_exist(
-        df: DataFrame,
-        collect_set_col: str):
-    tmp_col = f"{collect_set_col}_{uuid.uuid4().hex}"
-    df = df.withColumn(tmp_col, F.when((F.col(collect_set_col).isNotNull()) & (F.col(collect_set_col) != ""), F.col(collect_set_col)))
-    df = df.filter(
-        (F.size(F.col(tmp_col)) <= 2) &  # Ensure set size is not larger than 2
-        (
-                (F.size(F.col(tmp_col)) < 2) |  # If size is less than 2, include
-                (
-                        (F.size(F.col(tmp_col)) == 2) &  # If size is 2, check for None or empty string
-                        (F.array_contains(F.col(tmp_col), None) | F.array_contains(F.col(tmp_col), ""))
-                )
-        )
-    )
-    df = df.withColumn(collect_set_col,
-                       F.when(F.size(F.col(tmp_col)) == 1,
-                              F.explode(tmp_col))
-                       .otherwise(F.explode(F.array_except(F.lit(None))))
-                       )
-    return df
-def  reduce_into_valid_nutshell_start_transaction_events(
-        df: DataFrame,
-        a_col: str,
-        b_col: str,
-        status_col: str,
-        time_col: str,
-        other_b_cols: List[str]):
+    @staticmethod
+    def avoid_more_than_one_non_null_value_and_keep_the_non_null_if_exists(
+            df: DataFrame,
+            collect_set_col: str) -> DataFrame:
+        tmp_col = f"{collect_set_col}_{uuid.uuid4().hex}"
+        df = df.withColumn(tmp_col, F.col(collect_set_col))
+        df.show(truncate=False)
+        df = df.where(F.size(tmp_col) <= 1)
+        df = df.withColumn(collect_set_col,F.explode_outer(tmp_col)).drop(tmp_col)
+        return df
+    def  reduce_into_valid_nutshell_start_transaction_events(
+            self,
+            df: DataFrame,
+            a_col: str,
+            b_col: str,
+            status_col: str,
+            time_col: str,
+            other_b_cols: List[str]):
 
-    collect_set_cols =[b_col]+other_b_cols
-    agg_expressions = [F.collect_set(F.col(column)).alias(column) for column in collect_set_cols]
-    df_aggregated = df.groupBy(a_col, time_col, status_col).agg(*agg_expressions)
-    for col in collect_set_cols:
-        df_aggregated = keep_when_only_one_non_null_value_and_be_the_non_null_one_if_exist(df_aggregated, col)
-    return df
+            collect_set_cols =[b_col]+other_b_cols
+            agg_expressions = [F.collect_set(F.col(column)).alias(column) for column in collect_set_cols]
+            df_aggregated = df.groupBy(a_col, time_col, status_col).agg(*agg_expressions)
+            for col in collect_set_cols:
+                df_aggregated = self.avoid_more_than_one_non_null_value_and_keep_the_non_null_if_exists(df_aggregated, col)
+            return df
 
-def create_transaction_window(a_col: str, b_col: str, time_col: str, status_col:str):
-    return Window.partitionBy(a_col).orderBy(time_col, b_col, status_col)
+    def create_transaction_window(a_col: str, b_col: str, time_col: str, status_col:str) -> WindowSpec:
+        return Window.partitionBy(a_col).orderBy(time_col, b_col, status_col)
 
-def create_condition_end_after_start(
-        window_spec: WindowSpec, time_col: str, status_col:str):
-    end_after_start_cond = ((F.col(status_col) == Status.END.value) &
-                                   (F.lag(status_col, 1).over(window_spec) == Status.START))
-    time_is_after = F.col(time_col) > F.lag(time_col, 1).over(window_spec)
-    return end_after_start_cond & time_is_after
+    def create_condition_end_after_start(
+            self,
+            window_spec: WindowSpec, time_col: str, status_col:str):
+            end_after_start_cond = ((F.col(status_col) == self.Status.END.value) &
+                                   (F.lag(status_col, 1).over(window_spec) == self.Status.START))
+            time_is_after = F.col(time_col) > F.lag(time_col, 1).over(window_spec)
+            return end_after_start_cond & time_is_after
 
-def mark_end_time_with_ending_reason(df: DataFrame,
-                                   partition_by_col: str,
-                                   time_col: str,
-                                   match_col: str,
-                                   status_col:str):
-    start_time_col = 'start_time'
-    end_time_map_reason4time_col = 'end_time'
+    def mark_end_time_with_ending_reason(
+                                self,
+                                df: DataFrame,
+                                partition_by_col: str,
+                                time_col: str,
+                                match_col: str,
+                                status_col:str):
+        start_time_col = 'start_time'
+        end_time_map_reason4time_col = 'end_time'
 
-    window_spec: WindowSpec = (Window.partitionBy(partition_by_col).
+        window_spec: WindowSpec = (Window.partitionBy(partition_by_col).
                                orderBy(time_col, match_col, status_col))
-    start_after_start_cond = ((F.col(status_col) == Status.START.value) &
-                              (F.lead(status_col, 1).over(window_spec) == Status.START))
-    end_after_start_cond = ((F.col(status_col) == Status.START.value) &
-                              (F.lead(status_col, 1).over(window_spec) == Status.END))
-    next_match_is_null = F.lead(match_col, 1).over(window_spec).isNull()
-    next_match_is_not_null = ~next_match_is_null
-    same_match_col = F.col(match_col) == F.lead(match_col, 1)
-    match_broken = ~same_match_col
-    df.withColumn(start_time_col, F.when((start_after_start_cond) | (end_after_start_cond),
+        start_after_start_cond = ((F.col(status_col) == self.Status.START.value) &
+                              (F.lead(status_col, 1).over(window_spec) == self.Status.START))
+        end_after_start_cond = ((F.col(status_col) == self.Status.START.value) &
+                              (F.lead(status_col, 1).over(window_spec) == self.Status.END))
+        next_match_is_null = F.lead(match_col, 1).over(window_spec).isNull()
+        next_match_is_not_null = ~next_match_is_null
+        same_match_col = F.col(match_col) == F.lead(match_col, 1)
+        match_broken = ~same_match_col
+
+        df = df.withColumn(start_time_col, F.when((start_after_start_cond) | (end_after_start_cond),
                                          F.col(time_col)))
-    map_expected = F.create_map(F.lit(EndingTransactionReason.MATCH_EXPECTED_END), F.col(time_col))
-    df = df.withColumn(end_time_map_reason4time_col, F.when(
-        end_after_start_cond & same_match_col, map_expected))
-    map_end_for_unexpected_match = F.create_map(F.lit(EndingTransactionReason.MATCH_END_FOR_UNEXPECTED_MATCH), F.col(time_col))
-    df = df.withColumn(end_time_map_reason4time_col, F.when(
-        end_after_start_cond & match_broken & next_match_is_not_null, map_end_for_unexpected_match))
-    map_end_for_unexpected_match_null = F.create_map(F.lit(EndingTransactionReason.MATCH_END_FOR_UNEXPECTED_NULL), F.col(time_col))
-    df = df.withColumn(end_time_map_reason4time_col, F.when(
-        end_after_start_cond & match_broken & next_match_is_not_null, map_end_for_unexpected_match_null))
-    map_start_for_unexpected_match = F.create_map(F.lit(EndingTransactionReason.MATCH_UNEXPECTED_START_DIFFERENT_MATCH), F.col(time_col))
-    df = df.withColumn(end_time_map_reason4time_col, F.when(
-        start_after_start_cond & match_broken & next_match_is_not_null, map_start_for_unexpected_match))
-    map_start_for_unexpected_same_match = F.create_map(F.lit(EndingTransactionReason.MATCH_UNEXPECTED_START_SAME_MATCH), F.col(time_col))
-    df = df.withColumn(end_time_map_reason4time_col, F.when(
-        start_after_start_cond & same_match_col & next_match_is_not_null, map_start_for_unexpected_same_match))
+        map_expected = F.create_map(F.lit(self.EndingTransactionReason.MATCH_EXPECTED_END), F.col(time_col))
+        df = df.withColumn(end_time_map_reason4time_col, F.when(
+            end_after_start_cond & same_match_col, map_expected))
+        map_end_for_unexpected_match = F.create_map(F.lit(self.EndingTransactionReason.MATCH_END_FOR_UNEXPECTED_MATCH), F.col(time_col))
+        df = df.withColumn(end_time_map_reason4time_col, F.when(
+            end_after_start_cond & match_broken & next_match_is_not_null, map_end_for_unexpected_match))
+        map_end_for_unexpected_match_null = F.create_map(F.lit(self.EndingTransactionReason.MATCH_END_FOR_UNEXPECTED_NULL), F.col(time_col))
+        df = df.withColumn(end_time_map_reason4time_col, F.when(
+            end_after_start_cond & match_broken & next_match_is_not_null, map_end_for_unexpected_match_null))
+        map_start_for_unexpected_match = F.create_map(F.lit(self.EndingTransactionReason.MATCH_UNEXPECTED_START_DIFFERENT_MATCH), F.col(time_col))
+        df = df.withColumn(end_time_map_reason4time_col, F.when(
+            start_after_start_cond & match_broken & next_match_is_not_null, map_start_for_unexpected_match))
+        map_start_for_unexpected_same_match = F.create_map(F.lit(self.EndingTransactionReason.MATCH_UNEXPECTED_START_SAME_MATCH), F.col(time_col))
+        df = df.withColumn(end_time_map_reason4time_col, F.when(
+            start_after_start_cond & same_match_col & next_match_is_not_null, map_start_for_unexpected_same_match))
+        return df
 
-
-def prepare_matches(df: DataFrame,
+    def prepare_matches(
+                    self,
+                    df: DataFrame,
                     a_col: str,
                     b_col: str,
                     status_col: str,
                     time_col: str,
-                    status_values: Dict[str, Status],
+                    status_values: Dict[Status, str],
                     other_b_cols: List[str]) -> DataFrame:
-    df = initial_filter_non_nulls_a_col_time_col(df, a_col, time_col)
-    df = enumerate_status_column(df, status_col, status_values)
-    df = keep_only_start_end_status(df, status_col)
-    df = avoid_b_col_null_on_start_status(df, b_col, status_col)
-    df = reduce_into_valid_nutshell_start_transaction_events(df,a_col,b_col,status_col,time_col,other_b_cols)
-
-    #remove succeeding start status row for the same a_col,b_col
-    df = mark_end_time_with_ending_reason(df, a_col, time_col, b_col, status_col)
-    #
-    # a_to_b_window: WindowSpec = create_transaction_window(a_col,b_col, time_col)
-    # start_end_a_b_cond = create_condition_end_after_start(a_to_b_window,time_col, status_col)
-    #
-    # b_to_a_window: WindowSpec = create_transaction_window(b_col,a_col, time_col)
-    # start_end_b_a_cond = create_condition_end_after_start(b_to_a_window,time_col, status_col)
-    #
-    # df = df.withColumn('start_time_a_b', F.when(start_end_a_b_cond, F.lag(time_col,1).over(a_to_b_window)))
-    # df = df.withColumn('start_time_b_a', F.when(start_end_b_a_cond, F.lag(time_col,1).over(b_to_a_window)))
-
-
-
-    return df
+        df = self.initial_filter_non_nulls_a_col_time_col(df, a_col, time_col)
+        df = self.enumerate_status_column(df, status_col, status_values)
+        df = self.keep_only_start_end_status(df, status_col)
+        df = self.avoid_b_col_null_on_start_status(df, b_col, status_col)
+        df = self.reduce_into_valid_nutshell_start_transaction_events(df,a_col,b_col,status_col,time_col,other_b_cols)
+        df = self.mark_end_time_with_ending_reason(df, a_col, time_col, b_col, status_col)
+        return df
--- spark/find_match/tests/test_find_match_ranges.py
+++ spark/find_match/tests/test_find_match_ranges.py
@@ -1,24 +1,105 @@
-from pyspark.sql.types import StructType, StructField, StringType, IntegerType, Row
-
-from spark.find_match.find_match_ranges import initial_filter_non_nulls_a_col_time_col
-from tests.infra.dataframes_helpers import complete_row_to_schema, compare_dataframes
+from pyspark.sql.types import StructType, StructField, StringType, IntegerType, Row, ArrayType
+import pyspark.sql.functions as F
+from spark.find_match.find_match_ranges import FindMatchRange
+from tests.infra.dataframes_helpers import complete_row_to_schema, compare_dataframes, dataframe_create
 from tests.infra.default_spark_builder import DefaultSparkFactory
 
+spark = DefaultSparkFactory().spark
+
+find_match_ranges = FindMatchRange()
 def test_initial_filter_non_nulls_a_col_time_col():
     schema = StructType([
         StructField("a", StringType(), True),
         StructField("time", IntegerType(), True),
         StructField("other", StringType(), True),
     ])
-    spark = DefaultSparkFactory().spark
     rows = [Row(a="a", time=1),
             Row(a="a", time=None),
             Row(a=None, time=2),
             Row(a=None, time=None)]
-    completed_rows = complete_row_to_schema(rows, schema, complete_with_nulls=True)
-    df = spark.createDataFrame(completed_rows, schema)
-    df = initial_filter_non_nulls_a_col_time_col(df,a_col='a', time_col='time')
+    df =dataframe_create(rows, schema, complete_with_nulls=True)
+    df = find_match_ranges.initial_filter_non_nulls_a_col_time_col(df,a_col='a', time_col='time')
     rows_expected = [Row(a="a", time=1)]
     rows_expected = complete_row_to_schema(rows_expected, schema, complete_with_nulls=True)
     df_expected =  spark.createDataFrame(rows_expected, schema)
     compare_dataframes(df_expected,df)
+
+
+def test_enumerate_status_colum():
+    schema = StructType([
+        StructField("status", StringType(), True),
+        StructField("other", StringType(), True),
+    ])
+    rows = [Row(status="invalid"),
+            Row(status="start"),
+            Row(status="update"),
+            Row(status="end")]
+    df = dataframe_create(rows, schema, complete_with_nulls=True)
+    df = find_match_ranges.enumerate_status_column(df, status_col='status',status_values={
+         find_match_ranges.Status.INVALID: "invalid",
+         find_match_ranges.Status.START: "start",
+         find_match_ranges.Status.UPDATE : "update",
+         find_match_ranges.Status.END: "end"
+    })
+    schema = StructType([
+        StructField("status", IntegerType(), True),
+        StructField("other", StringType(), True),
+    ])
+    expected = dataframe_create([Row(status=i) for i in range(0,4)],
+                                     schema, complete_with_nulls=True)
+    compare_dataframes(expected, df)
+
+def test_keep_only_start_end_status():
+    schema = StructType([
+        StructField("status", IntegerType(), True),
+    ])
+    rows = [Row(status=find_match_ranges.Status.INVALID.value),
+            Row(status=find_match_ranges.Status.START.value),
+            Row(status=find_match_ranges.Status.UPDATE.value),
+            Row(status=find_match_ranges.Status.END.value)]
+    df = dataframe_create(rows, schema, complete_with_nulls=True)
+    df = find_match_ranges.keep_only_start_end_status(df, 'status')
+    expected = dataframe_create(
+        [Row(status=find_match_ranges.Status.START.value),
+         Row(status=find_match_ranges.Status.END.value)],
+         schema)
+    compare_dataframes(expected, df)
+
+def test_avoid_b_col_null_on_start_status():
+    schema = StructType([
+        StructField("status", IntegerType(), True),
+        StructField("b_col", StringType(), True),
+        StructField("other", StringType(), True),
+    ])
+    rows = [Row(status=find_match_ranges.Status.UPDATE.value, b_col='x'),
+            Row(status=find_match_ranges.Status.UPDATE.value, b_col=None),
+            Row(status=find_match_ranges.Status.START.value, b_col='x'),
+            Row(status=find_match_ranges.Status.START.value, b_col=None)]
+    df = dataframe_create(rows, schema)
+    df = find_match_ranges.avoid_b_col_null_on_start_status(df, b_col='b_col',status_col='status')
+    rows = [Row(status=find_match_ranges.Status.UPDATE.value, b_col='x'),
+            Row(status=find_match_ranges.Status.UPDATE.value, b_col=None),
+            Row(status=find_match_ranges.Status.START.value, b_col='x')]
+    expected =  dataframe_create(rows, schema)
+    compare_dataframes(expected, df)
+
+def test_avoid_more_than_one_non_null_value_and_keep_the_non_null_if_exists():
+    schema = StructType([StructField("id", IntegerType(), True),
+            StructField("col", ArrayType(StringType()), True)])
+
+    rows = [Row(id=1,col=['x1','x2','x3']),
+            Row(id=1,col=['x1','x2']),
+            Row(id=2,col=['x3']),
+            Row(id=3,col=[None])
+            ]
+    df = dataframe_create(rows, schema)
+    df.show(truncate=False)
+    df.printSchema()
+    df = find_match_ranges.avoid_more_than_one_non_null_value_and_keep_the_non_null_if_exists(df, collect_set_col='col')
+    df.printSchema()
+    rows = [Row(id=2,col='x3'),
+            Row(id=3 ,col=None)]
+    schema = StructType([StructField("id", IntegerType(), True),
+            StructField("col", StringType(), True)])
+    expected =  dataframe_create(rows, schema)
+    compare_dataframes(expected, df)
